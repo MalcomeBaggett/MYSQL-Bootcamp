a65aecf6f07901fca27158b2adc09d709edbdf00
Malcome Baggett <malcomebaggett@gmail.com>
2023-12-14 11:40:34 -0500

    Added more concat functions

--- StringFunctions/commands.sql
+++ StringFunctions/commands.sql
@@ -1,2 +1,10 @@
--- Concat two strings
-SELECT CONCAT('h','e','l',l,'o')
+-- Concat multiple strings
+SELECT CONCAT('h','e','l','l','o');
+
+-- concat columns
+SELECT CONCAT(author_fname,' ',author_lname) AS full_name FROM books;
+
+-- concat function with builtin seperator
+SELECT 
+    CONCAT_WS(' - ', title, author_fname, author_lname)
+FROM books;
