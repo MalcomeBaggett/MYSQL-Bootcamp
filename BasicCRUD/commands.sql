596f3084adabb67ad43379f1504ace8ef7157994
Malcome Baggett <malcomebaggett@gmail.com>
2023-12-18 14:23:56 -0500

    added first part of strings challenges

--- BasicCRUD/commands.sql
+++ BasicCRUD/commands.sql
@@ -5,63 +5,130 @@ CREATE TABLE cats (
     breed VARCHAR(100),
     age INT,
     PRIMARY KEY (cat_id)
-); 
+);
 
 -- Bulk insert into the the cats DB
-INSERT INTO cats(name, breed, age) 
-VALUES ('Ringo', 'Tabby', 4),
-       ('Cindy', 'Maine Coon', 10),
-       ('Dumbledore', 'Maine Coon', 11),
-       ('Egg', 'Persian', 4),
-       ('Misty', 'Tabby', 13),
-       ('George Michael', 'Ragdoll', 9),
-       ('Jackson', 'Sphynx', 7);
+INSERT INTO
+    cats(name, breed, age)
+VALUES
+    ('Ringo', 'Tabby', 4),
+    ('Cindy', 'Maine Coon', 10),
+    ('Dumbledore', 'Maine Coon', 11),
+    ('Egg', 'Persian', 4),
+    ('Misty', 'Tabby', 13),
+    ('George Michael', 'Ragdoll', 9),
+    ('Jackson', 'Sphynx', 7);
 
 -- Select all from a table
-SELECT * FROM cats;
+SELECT
+    *
+FROM
+    cats;
 
 -- Select column from a table
-SELECT  name FROM cats;
+SELECT
+    name
+FROM
+    cats;
 
 -- Select multiple specific columns
-SELECT name, breed FROM cats;
+SELECT
+    name,
+    breed
+FROM
+    cats;
 
 -- Select certain fields conditionally
-SELECT name, age FROM cats WHERE age = 4;
+SELECT
+    name,
+    age
+FROM
+    cats
+WHERE
+    age = 4;
 
 -- Select all by id
-SELECT id FROM cats;
+SELECT
+    id
+FROM
+    cats;
 
 -- Select name and breed 
-SELECT name, breed FROM cats;
+SELECT
+    name,
+    breed
+FROM
+    cats;
 
 -- Select name and age from cats who are of the breed tabby
-SELECT name, age FROM cats WHERE breed = "Tabby";
+SELECT
+    name,
+    age
+FROM
+    cats
+WHERE
+    breed = "Tabby";
 
 -- Select name and age where cat id = age
-SELECT name, age FROM cat WHERE cat_id = age;
+SELECT
+    name,
+    age
+FROM
+    cat
+WHERE
+    cat_id = age;
 
 -- Alias example
-SELECT cat_id as id, age, name FROM cat where cat_id = age;
+SELECT
+    cat_id as id,
+    age,
+    name
+FROM
+    cat
+where
+    cat_id = age;
 
 -- UPDATE, without a where clause it will be update all rows in the table
-UPDATE cats SET breed='Shorthair'
-WHERE breed='Tabby';
+UPDATE
+    cats
+SET
+    breed = 'Shorthair'
+WHERE
+    breed = 'Tabby';
 
-UPDATE cats SET age=14 WHERE name='Misty';
+UPDATE
+    cats
+SET
+    age = 14
+WHERE
+    name = 'Misty';
 
 -- Change name of a certain cat
-UPDATE cats SET name="Jack"
-WHERE name="Jackson";
+UPDATE
+    cats
+SET
+    name = "Jack"
+WHERE
+    name = "Jackson";
 
 -- Delete
-DELETE FROM  cats WHERE name="Egg";
+DELETE FROM
+    cats
+WHERE
+    name = "Egg";
 
 -- Delete all rows from a table
-DELETE FROM cats;
+DELETE FROM
+    cats;
 
 -- Delete all cats where age is 4
-DELETE FROM cats where age = 4;
+DELETE FROM
+    cats
+where
+    age = 4;
 
 -- Delete all cats whose id and age are the same
-DELETE FROM cats where age = cat_id;
+DELETE FROM
+    cats
+where
+    age = cat_id;
--- StringFunctions/commands.sql
+++ StringFunctions/commands.sql
@@ -1,10 +1,78 @@
 -- Concat multiple strings
-SELECT CONCAT('h','e','l','l','o');
+SELECT
+    CONCAT('h', 'e', 'l', 'l', 'o');
 
 -- concat columns
-SELECT CONCAT(author_fname,' ',author_lname) AS full_name FROM books;
+SELECT
+    CONCAT(author_fname, ' ', author_lname) AS full_name
+FROM
+    books;
 
 -- concat function with builtin seperator
-SELECT 
+SELECT
     CONCAT_WS(' - ', title, author_fname, author_lname)
-FROM books;
+FROM
+    books;
+
+-- Substrings
+SELECT
+    SUBSTRING('Hello World', 1, 4);
+
+-- if you only pass in one param it goes from the index until the end of the string
+SELECT
+    SUBSTRING('Hello World', 7);
+
+-- Negative numbers start from the end of the string
+SELECT
+    SUBSTRING('Hello World', -1);
+
+-- Use with columns form tables
+SELECT
+    SUBSTRING(title, 1, 15)
+FROM
+    books;
+
+SELECT
+    DISTINCT SUBSTRING(author_lname, 1, 1),
+    author_lname
+FROM
+    books;
+
+-- Combining string functions
+SELECT
+    CONCAT(SUBSTRING(title, 1, 10), '...') AS short_title
+FROM
+    books;
+
+SELECT
+    SUBSTRING(author_fname, 1, 1),
+    SUBSTRING(author_lname, 1, 1)
+FROM
+    books;
+
+SELECT
+    CONCAT(
+        SUBSTRING(author_fname, 1, 1),
+        '.',
+        SUBSTRING(author_lname, 1, 1),
+        '.'
+    ) AS initials
+FROM
+    books;
+
+-- replace functions
+SELECT
+    REPLACE('Hello World', 'Hell', '%$#@');
+
+-- -> %$#@o World
+SELECT
+    REPLACE('cheese bread coffee milk', ' ', ' and ');
+-- replace spaces with -
+SELECT REPLACE(title, ' ', '-') FROM books;
+
+-- reverse
+SELECT REVERSE('Hello World');
+
+-- Length
+SELECT CHAR_LENGTH("Hello");
+SELECT CHAR_LENGTH(title), title FROM books;
